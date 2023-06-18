ad6f148732fb3f11fbab7324c087b678e3973ec9
Chiara Iannetta <chiara.iannetta@beije.it>
2023-06-18 16:39:55 +0200

    Correct TestMethods.java

--- Manuale/src/it/beije/xvii/exercises/iannetta/TestMethods.java
+++ Manuale/src/it/beije/xvii/exercises/iannetta/TestMethods.java
@@ -3,33 +3,16 @@ package it.beije.xvii.exercises.iannetta;
 import java.util.Scanner;
 
 public class TestMethods {
-
-	public static String name; 
 	
-	public static void setter(String s) {
-		name = s;
-	}
-	
-	public static String getter() {
-		return name;
-	}
 	public static void main(String[] args) {
 		
-		System.out.println("Name: " + name);
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter a new name: ");
-		String string = in.nextLine();
+		System.out.println("Enter a new name: ");		
+		String name = in.nextLine();
 		in.close();
-		System.out.println("Setting new name");
-		setter(string);
-		System.out.println("Getter: " + getter());
-		
-		
-		
-		
-		
-		
-		
+		String setter = "set" + (name.charAt(0) + "").toUpperCase() + name.substring(1).toLowerCase(); 
+		System.out.println("Setter name: " + setter);
+		String getter = "get" + (name.charAt(0) + "").toUpperCase() + name.substring(1).toLowerCase(); 
+		System.out.println("Getter name: " + getter);	
 	}
-
 }
