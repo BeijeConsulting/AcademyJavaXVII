5974c942146842aca1aaa7326c78a1950ca8d0c0
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-06-20 16:54:19 +0200

    funzione esercizio caffeina

--- Exercises/src/it/beije/xvii/exercises/Char/EsProgCompleti.java
+++ Exercises/src/it/beije/xvii/exercises/Char/EsProgCompleti.java
@@ -3,7 +3,8 @@ package it.beije.xvii.exercises.Char;
 public class EsProgCompleti {
 	
 	public static void main(String[] args) {
-		fabbricaBibita(700,8,30);
+//		fabbricaBibita(700,8,30);
+		caffeina(6);
 	
 	}
 	public static void fabbricaBibita(int content, int evapPerDay, int threshold) {
@@ -20,5 +21,21 @@ public class EsProgCompleti {
             days++;
         }
 
+	}
+	public static void caffeina(int n) {
+		if(n % 3 == 0 && n % 4 != 0) {
+			System.out.print("Java");
+			if(n % 2 == 0)
+				System.out.print("Script");
+		}
+			
+		else if(n % 3 == 0 && n % 4 == 0) {
+			System.out.print("Coffee");
+			if(n % 2 == 0)
+				System.out.print("Script");
+		}
+		else 
+			System.out.println("match_missed!");
+			
 	}
 }
