8f1e935c7036910d4333eb426f999766b8ecf069
Lara Mancuso <lara.mancuso@beije.it>
2023-06-21 15:09:43 +0200

    Aggiunta interfaccia amministra a prof ita

--- Exercises/src/it/beije/xvii/hierarchy/Ceccarelli_Char_Mancuso/ProfItaliano.java
+++ Exercises/src/it/beije/xvii/hierarchy/Ceccarelli_Char_Mancuso/ProfItaliano.java
@@ -1,6 +1,6 @@
 package it.beije.xvii.hierarchy.Ceccarelli_Char_Mancuso;
 
-public class ProfItaliano extends Professore{
+public class ProfItaliano extends Professore implements Amministra{
 	public void insegna() {
 		System.out.println("Sto insegnando italiano.");
 	}
