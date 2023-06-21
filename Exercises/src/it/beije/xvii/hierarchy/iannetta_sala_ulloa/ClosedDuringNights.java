f15018615a6d022ea33e40cdfbe326d51bb6d10d
Chiara Iannetta <chiara.iannetta@beije.it>
2023-06-21 15:05:31 +0200

    Classes and interfaces

---
+++ Exercises/src/it/beije/xvii/hierarchy/iannetta_sala_ulloa/ClosedDuringNights.java
@@ -0,0 +1,5 @@
+package it.beije.xvii.hierarchy.iannetta_sala_ulloa;
+
+public interface ClosedDuringNights {
+	public void closedDuringNights();
+}
---
+++ Exercises/src/it/beije/xvii/hierarchy/iannetta_sala_ulloa/Stairs.java
@@ -0,0 +1,5 @@
+package it.beije.xvii.hierarchy.iannetta_sala_ulloa;
+
+public interface Stairs {
+	public void stairs();
+}
