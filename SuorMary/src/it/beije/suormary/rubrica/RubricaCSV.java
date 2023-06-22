7bad2328964dfe0093be1818aca11d3b6006674c
ivfly <iv0fly@gmail.com>
2023-06-22 14:51:52 +0200

    + FileWriter

--- SuorMary/src/it/beije/suormary/rubrica/RubricaCSV.java
+++ SuorMary/src/it/beije/suormary/rubrica/RubricaCSV.java
@@ -1,13 +1,15 @@
 package it.beije.suormary.rubrica;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.lang.reflect.Array;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
+
 
 public class RubricaCSV {
 
@@ -39,8 +41,15 @@ public class RubricaCSV {
 			//System.out.println(r);
 		}
 		
+		bufferedReader.close();
+		fileReader.close();
+		
+		
 		System.out.println("rows number: " + rows.size());
 		
+		
+		FileWriter fileWriter = new FileWriter("/temp/appoggio/prova.csv");
+	
 		for (String row : rows) {
 //			StringTokenizer tokenizer = new StringTokenizer(row, ";");
 //			while (tokenizer.hasMoreElements()) {
@@ -48,10 +57,20 @@ public class RubricaCSV {
 //			}
 //			System.out.println("---");
 			
+			row = row.substring(1,row.length()-1);
 			String[] contact = row.split("\";\"");
 			System.out.println(Arrays.toString(contact));
-
+			
+			for (int j=4; j > 0; j--) {
+				fileWriter.write(contact[j]);
+				fileWriter.write('|');
+			}
+			fileWriter.write(contact[0]);
+			fileWriter.write('\n');
+			fileWriter.flush();
 		}
+		
+		fileWriter.close();
 		
 		System.out.println("FINE");
 	}
