4430da2ac240ec9a806d178139a34733fb54fe65
Lara Mancuso <lara.mancuso@beije.it>
2023-06-27 16:34:31 +0200

    continuazione jdbc

--- Exercises/src/it/beije/xvii/exercises/mancuso/RubricaJDBC.java
+++ Exercises/src/it/beije/xvii/exercises/mancuso/RubricaJDBC.java
@@ -2,26 +2,53 @@ package it.beije.xvii.exercises.mancuso;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class RubricaJDBC {
 
 	public static void main(String[] args) {
-		
+		Connection connection = null;
+		Statement statement = null;
 		try {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost::3306/suor_mary?serverTimezon=CET", "root", "myDatabase1");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost::3306/suor_mary?serverTimezon=CET", "root", "myDatabase1");
 			
-			Statement statement = connection.createStatement();
+			statement = connection.createStatement();
 			
 			//SELECT
-			statement.executeQuery("SELECT * FROM rubrica");
+			ResultSet rs = statement.executeQuery("SELECT * FROM rubrica");
 			
+			while(rs.next()) {
+				/*System.out.println("id : " + rs.getInt(1));
+				System.out.println("nome : " + rs.getString(2));
+				System.out.println("cognome : " + rs.getString(3));
+				System.out.println("telefono : " + rs.getString(4));
+				System.out.println("email : " + rs.getString(5));
+				System.out.println("note : " + rs.getString(6));*/
+				
+				System.out.println("id : " + rs.getInt("id"));
+				System.out.println("nome : " + rs.getString("nome"));
+				System.out.println("cognome : " + rs.getString("cognome"));
+				System.out.println("telefono : " + rs.getString("telefono"));
+				System.out.println("email : " + rs.getString("email"));
+				System.out.println("note : " + rs.getString("note"));
+				
+				System.out.println("------------------");
+			}
+			rs.close();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
+			try {
+				statement.close();
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 			
 		}
 		
