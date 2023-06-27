b4ad425f5634a4e3240081ff36ec9d7243f30738
Alice Ceccarelli <alice.ceccarelli11@gmail.com>
2023-06-27 16:48:12 +0200

    jdbc

--- Exercises/src/it/beije/xvii/exercises/ceccarelli/Esercizi_file/Rubrica/Rubrica_JDBC.java
+++ Exercises/src/it/beije/xvii/exercises/ceccarelli/Esercizi_file/Rubrica/Rubrica_JDBC.java
@@ -2,6 +2,7 @@ package it.beije.xvii.exercises.ceccarelli.Esercizi_file.Rubrica;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,23 +10,43 @@ public class Rubrica_JDBC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Connection connection = null;
+		Statement statement = null;
 		try {
 			
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306//alice_ceccarelli?serverTimezone=CET", "root","Ali1196");
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306//alice_ceccarelli?serverTimezone=CET", "root","Ali1196");
 		
-		Statement statement = connection.createStatement();
+		statement = connection.createStatement();
 		System.out.println("connection open? " + !connection.isClosed());
-		//SELECT
-		statement.executeQuery("SELECT * FROM rubrica");
 		
+		//SELECT
+		ResultSet rs = statement.executeQuery("SELECT * FROM rubrica");
+		while(rs.next()) {
+			System.out.println("id: " + rs.getString("id"));
+			System.out.println("nome: " + rs.getString("nome"));
+			System.out.println("cognome: " + rs.getString("cognome"));
+			System.out.println("telefono: " + rs.getString("telefono"));
+			System.out.println("email: " + rs.getString("email"));
+			System.out.println("note: " + rs.getString("note"));
+			
+			System.out.println("----------------");
+		}
+		
+		rs.close();
 		}catch(ClassNotFoundException e) {
 			e.printStackTrace();
 		}catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			
+			try {
+				statement.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
