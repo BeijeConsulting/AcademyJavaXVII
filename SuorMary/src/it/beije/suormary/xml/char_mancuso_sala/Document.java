5f9b55aa15a3dbbb39d28fc811915efda2618517
Ibrahim Char <ibrachar68@gmail.com>
2023-06-30 12:33:58 +0200

    implementazione metodo parse

--- SuorMary/src/it/beije/suormary/xml/char_mancuso_sala/Document.java
+++ SuorMary/src/it/beije/suormary/xml/char_mancuso_sala/Document.java
@@ -2,12 +2,15 @@ package it.beije.suormary.xml.char_mancuso_sala;
 
 import java.util.List;
 
-public class Document {
+public class Document  {
 	
 	public String document;
 	
 	public Element getRootElement() {
 		return null;
+	}
+	public static Document parse(String file) {		
+		return XMLUtilities.readXML(file);
 	}
 	
 }
