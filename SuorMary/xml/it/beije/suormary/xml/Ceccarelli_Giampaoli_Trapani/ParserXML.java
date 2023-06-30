921b94a0201a287a6ee6d96d037998477587133d
Martina Trapani <martitrapani1999gmail.com>
2023-06-30 09:40:10 +0200

    parser

---
+++ SuorMary/xml/it/beije/suormary/xml/Ceccarelli_Giampaoli_Trapani/ParserXML.java
@@ -0,0 +1,10 @@
+package it.beije.suormary.xml.Ceccarelli_Giampaoli_Trapani;
+
+public class ParserXML {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
