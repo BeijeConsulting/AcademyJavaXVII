d58bee69e5a29917664258cd3c5bb42f6fbc4ea5
ivfly <iv0fly@gmail.com>
2023-07-05 14:45:41 +0200

    + Singleton

---
+++ SuorMary/src/it/beije/suormary/rubrica/Runner.java
@@ -0,0 +1,13 @@
+package it.beije.suormary.rubrica;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Singleton.getInstance());
+		System.out.println(Singleton.getInstance());
+		System.out.println(Singleton.getInstance());
+
+	}
+
+}
