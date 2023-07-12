12426f6dd28e1e4825a2f1e7f5521b5bd85cf936
Ibrahim Char <ibrachar68@gmail.com>
2023-07-12 12:40:00 +0200

    Merge remote-tracking branch 'origin/main' into webChar

--- SuorMaryWeb/src/it/beije/suormary/web/Char/Contact.java
+++ SuorMaryWeb/src/it/beije/suormary/web/Char/Contact.java
@@ -1,11 +1,15 @@
 package it.beije.suormary.web.Char;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "rubrica")
@@ -18,12 +22,25 @@ public class Contact {
 	private String name;
 	@Column
     private String surname;
-    @Column
-    private String email;
-    @Column(name = "phone")
-    private String phoneNumber;
+//    @Column
+//    private String email;
+//    @Column(name = "phone")
+//    private String phoneNumber;
     @Column
     private String note;
+    @Transient
+    private List<ContactDetail> contactDetails; 
+    public Contact() {
+    	contactDetails = new ArrayList<>();
+    }
+    
+	public List<ContactDetail> getContactDetails() {
+		return contactDetails;
+	}
+
+	public void addContactDetail(ContactDetail contactDetail) {
+		contactDetails.add(contactDetail);
+	}
     
     public int getId() {
  		return id;
@@ -48,21 +65,21 @@ public class Contact {
 		this.surname = surname;
 	}
 	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//	
+//	public String getPhoneNumber() {
+//		return phoneNumber;
+//	}
+//	
+//	public void setPhoneNumber(String phoneNumber) {
+//		this.phoneNumber = phoneNumber;
+//	}
 	
 	public String getNote() {
 		return note;
@@ -72,13 +89,11 @@ public class Contact {
 		this.note = note;
 	}
 	public String toString() {
-		return "Nome : " + getName() + " - " + "Cognome : " + getSurname() + " - " + "Email : " + getEmail() + " - " + "Telefono : " + getPhoneNumber() + " - " + "Note : " + getNote();
+		return "Nome : " + getName() + " - " + "Cognome : " + getSurname() +  " - " + "Note : " + getNote();
 	}
 	public boolean equals(Contact c2) {
 		if(this.getName().equals(c2.getName())&& 
 		   this.getSurname().equals(c2.getSurname()) && 
-		   this.getEmail().equals(c2.getEmail()) && 
-		   this.getPhoneNumber().equals(c2.getPhoneNumber())&&
 		   this.getNote().equals(c2.getNote())) return true;
 		else return false;
 	    
