e97c7ec3c3b38abf46b5ffdfa3237f98a12d448f
Lara Mancuso <lara.mancuso@beije.it>
2023-07-14 12:20:03 +0200

    creazione package

---
+++ SuorMaryWeb/src/it/beije/suormary/bookstore1/OrderItem.java
@@ -0,0 +1,101 @@
+package it.beije.suormary.bookstore1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_id_fk_idx` (`order_id`),
+  KEY `book_id_fk_idx` (`book_id`),
+  CONSTRAINT `book_id_fk` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `order_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB
+
+*/
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "order_id")
+	private int orderId;
+
+	@Column(name = "book_id")
+	private int bookId;
+
+	@Column(name = "price")
+	private double price;
+
+	@Column(name = "quantity")
+	private int quantity;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder("{ ")
+				.append("id : ").append(id)
+				.append(", orderId : ").append(orderId)
+				.append(", bookId : ").append(bookId)
+				.append(", price : ").append(price)
+				.append(", quantity : ").append(quantity)
+				.append(" }");
+		
+		return builder.toString();
+	}	
+}
