b0d612f3b755dfa6cdf503ed416af712f1d58c2d
Chiara Iannetta <chiara.iannetta@beije.it>
2023-07-14 13:03:20 +0200

    Home

---
+++ SuorMaryWeb/WebContent/loginpage.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Login</title>
+</head>
+
+<body>
+<h1>SEI NEL LOGIN</h1>
+
+<div>
+	<form>
+		<label for="username"></label>
+		
+	</form>
+</div>
+
+</body>
+
+
+</html>
---
+++ SuorMaryWeb/src/it/beije/suormary/bookstore4_ceccarelli_iannetta/HomeServlet.java
@@ -0,0 +1,51 @@
+package it.beije.suormary.bookstore4_ceccarelli_iannetta;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/homeservlet")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HomeServlet doGet");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HomeServlet doPost");
+		
+		HttpSession session = request.getSession();
+
+		String username = (String) request.getParameter("username");
+		
+		if (username != null) { //logged
+			response.sendRedirect("listpage.jsp");
+		} else { //non logged
+			response.sendRedirect("loginpage.jsp");
+		}
+	}
+
+}
