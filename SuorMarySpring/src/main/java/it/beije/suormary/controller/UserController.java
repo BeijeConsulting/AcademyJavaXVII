22b9a6440bb763e362f238cd853a0b062ca125c5
Ibrahim Char <ibrachar68@gmail.com>
2023-07-19 11:50:10 +0200

    create e update book usando i service

--- SuorMarySpring/src/main/java/it/beije/suormary/controller/UserController.java
+++ SuorMarySpring/src/main/java/it/beije/suormary/controller/UserController.java
@@ -11,16 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import it.beije.suormary.service.UserService;
+
 @Controller
 public class UserController {
-	
-
+	@Autowired
+	  private UserService userService;
+ 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginGet(HttpSession session) {
 		if(session.getAttribute("email") != null) {
@@ -39,7 +43,7 @@ public class UserController {
 			return "welcome";
 		}
 		System.out.println(email);
-		User user = BookStoreUtility.loginUser(email);
+		User user = userService.loginUser(email);
 		if(user != null) {
 		    session.setAttribute("email", email);
 		    model.addAttribute("email", email);	    
@@ -70,7 +74,7 @@ public class UserController {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		LocalDateTime date = LocalDateTime.now();
-		BookStoreUtility.registerUser(name, surname, email, password, date);
+		userService.registerUser(name, surname, email, password, date);
 		session.setAttribute("email", email);
 		return "welcome";
 	}
