7b625cb1dc350e17a3c715595ed6a1ddbbb0d40c
Lara Mancuso <lara.mancuso@beije.it>
2023-08-09 10:05:11 +0200

    login :)

--- SuorMarySpring/src/main/java/it/beije/suormary/bookstore1/controller/UserRestController.java
+++ SuorMarySpring/src/main/java/it/beije/suormary/bookstore1/controller/UserRestController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.beije.suormary.bookstore1.model.User;
 import it.beije.suormary.bookstore1.service.UserService;
+import it.beije.suormary.dto.AuthCredentials;
+
 
 @RestController
+@RequestMapping(value = "/api")
 public class UserRestController {
 	
 	@Autowired
@@ -38,4 +42,18 @@ public class UserRestController {
 		return user;
 	}
 	
+	@PostMapping(value = "/login")
+	public String loginApi(@RequestBody AuthCredentials login) {
+		User user = userService.checkUser(login.getEmail(), login.getPassword());
+		if(user != null) {
+			return "Login avvenuto";
+		}
+		return "Login fallito";
+	}
+	
+	@GetMapping(value = "/logout")
+	public String logoutApi() {
+		//Passare la sessione da cancellare????
+		return "boh";
+	}
 }
