8df5aa008d06b6a1648f00fdfdd27d44b1e0b36c
Flaviana <flaviana.caroselli@beije.it>
2023-09-28 14:23:09 +0200

    aggiunta metodo in ex 9

--- Lessons/Stream/Demo/Stream/src/Esercizi/Ex_9.java
+++ Lessons/Stream/Demo/Stream/src/Esercizi/Ex_9.java
@@ -12,7 +12,7 @@ import Demo.Persona;
  *2- Raggruppare le persone in base all'attributo booleano diMare della classe Citta creata precedentemente utilizzando lo stream()
  *	 e raccogli in una nuova lista gli elementi filtrati.
  *
- *3- utilizza lo stream con le classi Persona e Citta' per ottenere una lista di tutte le città uniche
+ *3- Utilizza lo stream con le classi Persona e Citta' per ottenere una lista limitata di 3 città uniche
  * 	 in cui vive almeno una persona di età superiore a 30 anni e raccogli in una nuova lista gli elementi filtrati.
  * */
 
