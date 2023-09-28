a8028e6e79d5f33db064c7eda88130d82f671dc5
Martina Trapani <martitrapani1999gmail.com>
2023-09-28 13:00:57 +0200

    Esercizio 4

--- Lessons/JUnit/src/esercizi/esercizio4/GroupComparison.java
+++ Lessons/JUnit/src/esercizi/esercizio4/GroupComparison.java
@@ -1,5 +1,28 @@
 package esercizi.esercizio4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupComparison {
+	
+	//4
+	public Person[] relatives(Person[] people, Person p1) {
+		
+		if(people==null || p1==null) {
+			return null;
+		}
+		
+		
+		List<Person> relatives = new ArrayList<>(); 
+		
+		for(Person person : people) {
+			if(p1.getSurname().equals(person.getSurname())) {
+				relatives.add(person);
+			}
+		}	
+		return (Person[]) relatives.toArray();	
+	}
+	
+	
 
 }
--- Lessons/JUnit/src/esercizi/esercizio4/PeopleComparison.java
+++ Lessons/JUnit/src/esercizi/esercizio4/PeopleComparison.java
@@ -2,8 +2,16 @@ package esercizi.esercizio4;
 
 public class PeopleComparison {
 	
+
+	//2 - 2.1
+	public Person oldestPerson(Person p1, Person p2) {
+		return null;
+	}
 	
-	
-	
+	//3
+	public boolean sameCity(Person p1, Person p2) {	
+		return p1.getCity().equals(p2.getCity());
+	}
+		
 
 }
