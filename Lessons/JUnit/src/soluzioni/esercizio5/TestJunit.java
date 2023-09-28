f24b0d5447ef5c6600e803d90ebc5958ed3f5c2b
Chiara Iannetta <chiara.iannetta@beije.it>
2023-09-28 09:47:02 +0200

    Aggiunta casi di test

--- Lessons/JUnit/src/soluzioni/esercizio5/TestJunit.java
+++ Lessons/JUnit/src/soluzioni/esercizio5/TestJunit.java
@@ -2,47 +2,37 @@ package soluzioni.esercizio5;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestJunit {
 	
-	String[][] allTests = {{},
-						   {},
-						   {},
-						   {},
-						   {},
-						   {},
-						   {},
-						   {},
-						   {},
-						   {},
-						   {},
-						   {},
-						   {},
-							};
+	String[][] allTests = {{"S", "S", "N", "E"},
+						   {"S", "N"},
+						   {"E"},
+						   {"W", "W", "N", "S", "N"},
+						   {"W", "E", "E", "E", "S"},
+						   {"N", "N", "E", "W", "S"},
+						   {"E", "E", "W", "E", "S","N", "W", "E", "E", "S"}};
 	
-	String[][] allSolutions = {{},
-							   {},
-							   {},
-							   {},
-							   {},
-							   {},
-							   {},
-							   {},
-							   {},
-							   {},
-							   {},
-							   {},
-							   {},
-								};
+	String[][] allSolutions = {{"S", "E"},
+							   {""},
+							   {"E"},
+							   {"W", "W", "N"},
+							   {"E", "E", "S"},
+							   {"N"},
+							   {"E", "E", "E", "S"}};
+	
 	OptimisePath op = new OptimisePath();
 	
-	@Ignore
 	@Test
 	public void test() {
 		for (int i  = 0; i < allTests.length; i++) {
-			
+//			System.out.println("ehi");
+//			System.out.println(Arrays.toString(allSolutions[i]));
+//			System.out.println(Arrays.toString(op.optimise(allTests[i])));
 			assertArrayEquals(allSolutions[i], op.optimise(allTests[i]));
 		}
 	}
@@ -54,14 +44,9 @@ public class TestJunit {
 	
 	@Test
 	public void emptyTest() {
-		assertNull(op.optimise(new String[] {}));
-	}
-
-	@Test
-	public void emptyTest2() {
-		assertNull(op.optimise(new String[] {""}));
-		assertNull(op.optimise(new String[] {"", ""}));
-		assertNull(op.optimise(new String[] {"", ""}));
-		assertNull(op.optimise(new String[] {"", ""}));
+		assertArrayEquals(new String[] {}, op.optimise(new String[] {""}));
+		assertArrayEquals(new String[] {}, op.optimise(new String[] {"", ""}));
+		assertArrayEquals(new String[] {}, op.optimise(new String[] {"", "", ""}));
+		assertArrayEquals(new String[] {}, op.optimise(new String[] {"", "", "", ""}));
 	}
 }
