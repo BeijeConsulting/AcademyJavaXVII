d3aa6b262252095fa6a13d97ec7e97551848ce2b
Flaviana <flaviana.caroselli@beije.it>
2023-09-28 12:10:05 +0200

    ex9

--- Lessons/Stream/Demo/Stream/src/Esercizi/Ex_9.java
+++ Lessons/Stream/Demo/Stream/src/Esercizi/Ex_9.java
@@ -1,5 +1,23 @@
 package Esercizi;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/*
+ * Raggruppare, utilizzando lo stream(), le persone in base all'attributo citta'
+ * Raggruppare le persone in base all'attributo booleano diMare della classe Citta creata precedentemente utilizzando lo stream()
+ * */
+
 public class Ex_9 {
 
+    public static Map<Persona, Citta> personsByCity(List<Persona> inputList) {
+        return inputList.stream().collect(Collectors.toMap(p -> p, p -> p.getCitta()));
+    }
+
+    public static Map<Citta, List<Persona>> personsByCityOfSea(List<Persona> inputList) {
+        return inputList.stream().collect(Collectors.groupingBy(p -> p.getCitta()));
+    }
+    
+    
 }
