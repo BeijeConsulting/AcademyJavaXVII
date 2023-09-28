484695eac884e8cc1a5f06ba3926479411d771b9
Alice Ceccarelli <alice.ceccarelli11@gmail.com>
2023-09-28 11:46:26 +0200

    spiegazioni

--- Lessons/Stream/Demo/Stream/src/Soluzioni/Ex_1_soluzione.java
+++ Lessons/Stream/Demo/Stream/src/Soluzioni/Ex_1_soluzione.java
@@ -1,15 +1,25 @@
 package Soluzioni;
 
 import java.util.List;
+
+/*
+ * Filtraggio semplice: 
+ * Crea uno stream di numeri interi e filtra solo i numeri pari.
+ * Esempio: [1, 2, 3, 4, 5, 6] -> [2, 4, 6]
+*/
+
+
 import java.util.stream.Collectors;
 
 public class Ex_1_soluzione {
 	
 	public static List<Integer> ex1(List<Integer> integersList) {
 	
-	List<Integer> evenNumbers = integersList.stream()
-            .filter(x -> x % 2 == 0)
-            .collect(Collectors.toList());
+	List<Integer> evenNumbers = integersList.stream() // crea uno stream a partire dalla lista di input "integersList"
+            .filter(x -> x % 2 == 0) // il metodo filter viene usato per selezionare solo gli elementi che soddisfano la condizione specificata
+            .collect( // collect serve per raccogliere in una nuova lista gli elementi filtrati. 
+            		Collectors.toList() // Collectors.toList() è utilizzato per creare una lista contenente gli elementi filtrati.
+            		); 
     return evenNumbers;
 	}
 
--- Lessons/Stream/Demo/Stream/src/Soluzioni/Ex_2_soluzione.java
+++ Lessons/Stream/Demo/Stream/src/Soluzioni/Ex_2_soluzione.java
@@ -3,12 +3,19 @@ package Soluzioni;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
+ * Dato uno stream di stringhe, trasforma ogni stringa in maiuscolo.
+ * Esempio: ["a", "b", "c"] -> ["A", "B", "C"]
+ */
+
 public class Ex_2_soluzione {
 	
 	public List<String> ex2(List<String> stringsList) {
-        List<String> uppercaseStrings = stringsList.stream()
-                .map(x -> x.toUpperCase())
-                .collect(Collectors.toList());
+        List<String> uppercaseStrings = stringsList.stream() // crea uno stream a partire dalla lista di input "stringsList"
+                .map(x -> x.toUpperCase()) //  il metodo map serve per convertire ogni elemento della lista in maiuscolo utilizzando il metodo toUpperCase().
+                .collect( // collect serve per raccogliere in una nuova lista gli elementi filtrati.
+                		Collectors.toList() // Collectors.toList() è utilizzato per creare una lista contenente gli elementi filtrati.
+                		);
         return uppercaseStrings;
         
     }
