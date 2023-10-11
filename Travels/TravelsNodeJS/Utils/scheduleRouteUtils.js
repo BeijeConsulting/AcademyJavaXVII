fd8eafe4a5f95522aa465ba220fb36eea1d2c73c
Flaviana <flaviana.caroselli@beije.it>
2023-10-11 16:42:29 +0200

    schedules

--- Travels/TravelsNodeJS/Utils/scheduleRouteUtils.js
+++ Travels/TravelsNodeJS/Utils/scheduleRouteUtils.js
@@ -73,9 +73,9 @@ module.exports = {
             });
         }))
     },
-    getScheduleByRoute: function (route) {
+    getScheduleByRoute: function (routeId) {
         return new Promise((resolve, reject => {
-            connection.query("SELECT * FROM schedules WHERE route_id = ?", [route], (err, rows, fields) => {
+            connection.query("SELECT * FROM schedules WHERE route_id = ?", [routeId], (err, rows, fields) => {
                 if(err){
                     reject(err);
                 } else {
@@ -84,9 +84,116 @@ module.exports = {
             });
         }))
     },
-    getAllRoutesByCityNameLike: function (cityName) {
+    getSchedulesByRouteOnlyActive: function (routeId) {
+        //prendo la data di oggi
+        const currentDate = new Date().toISOString().slice(0, 10);
         return new Promise((resolve, reject) => {
-            connection.query("SELECT * FROM routes WHERE city_name LIKE ?", [cityName], (err, rows, fields) => {
+            connection.query("SELECT * FROM schedules WHERE route_id = ? AND (end_date is null or end_date <= ?)", [routeId, currentDate], (err, rows, fields) => {
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        })
+    },
+
+    getSchedulesByRouteAndDayAndValidity: function(routeId, days, departureDate) {
+
+        //days e' una lista di interi che rappresentano i giorni della settimana
+
+        return new Promise((resolve, reject) => {
+            connection.query('SELECT DISTINCT s1.* FROM schedules AS s1 ' +
+            'INNER JOIN days_of_week AS dow ON dow.schedule_id = s1.id ' +
+            'WHERE s1.route_id = ? AND dow.day IN (?) AND start_date <= ? AND end_date >= ?', [routeId, days, departureDate, departureDate], (err, rows, fields) => {
+                if(err){
+                    reject(err);
+                } else {
+                    let schedules = [];
+                    for(let i = 0; i < rows.length; i++){
+                        dayOfWeek.getDaysOfWeekBySchedule(rows[i]).then((days) => {
+                            for(let j = 0; j < days.length; j++){
+                                if(days[j].day == dayOfWeek){
+                                    schedules.push(rows[i]);
+                                }
+                            }
+                        })
+                    }
+                    resolve(schedules);
+                }
+            });
+        })
+    },
+    getSchedulesByIds: function (ids) {
+        return new Promise((resolve, reject) => {
+            connection.query("SELECT * FROM schedules WHERE id IN (?)", [ids], (err, rows, fields) => {
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        })
+    },
+    getSchedulesIdFromList: function (schedules) {
+        let ids = [];
+        for(let i = 0; i < schedules.length; i++){
+            ids.push(schedules[i].id);
+        }
+        return ids;
+    },
+
+    getAllRoutes: function () {
+        return new Promise((resolve, reject) => {
+            connection.query("SELECT * FROM routes", (err, rows, fields) => {
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        })
+    },
+    getRouteById: function (id) {
+        return new Promise((resolve, reject) => {
+            connection.query("SELECT * FROM routes WHERE id = ?", [id], (err, rows, fields) => {
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        })
+    },
+
+    // getRoutesByDepartureAndArrivalCityName: function (departure_city,arrival_city) {
+
+    // }
+
+    // getRoutesByDepartureCityName: function (departure_city) {
+
+    // }
+    // getAllRoutesByCityNameLike: function (departure_city, arrival_city) {
+    //mi servono glli id delle due citta passate
+    //poi devo trovare gli xport che hanno quei city id
+    //poi devo trovare le routes che hanno quegli xport id e passarsi alla query
+    //     return new Promise((resolve, reject) => {
+    //         connection.query(
+    //             "", [departure_city, arrival_city], (err, rows, fields) => {
+    //             if(err){
+    //                 reject(err);
+    //             } else {
+    //                 resolve(rows);
+    //             }
+    //         });
+    //     });
+    // },
+    getAllRoutesByXportNameLike: function (xportName) {
+        return new Promise((resolve, reject) => {
+            connection.query('SELECT * FROM routes' +
+            'INNER JOIN xports AS departureXport ON routes.departure_xport_id = departureXport.id ' +
+            'INNER JOIN xports AS arrivalXport ON routes.arrival_xport_id = arrivalXport.id ' +
+            'WHERE departureXport.name = ? AND arrivalXport.name = ?', [xportName], (err, rows, fields) => {
                 if(err){
                     reject(err);
                 } else {
@@ -94,5 +201,10 @@ module.exports = {
                 }
             });
         });
-    }
+    },
+    getRoutesByArrCityName
+
+
+        
+
 }
