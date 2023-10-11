dc7c1ff5ea01915eadff6bcd6f1adff6d10ad6ec
Flaviana <flaviana.caroselli@beije.it>
2023-10-11 15:27:21 +0200

    scheduleByRoute

--- Travels/TravelsNodeJS/Utils/scheduleRouteUtils.js
+++ Travels/TravelsNodeJS/Utils/scheduleRouteUtils.js
@@ -1,20 +1,17 @@
 const myModule = require("../mysql");
+const dayOfWeek = require("./dayOfWeekUtils");
 
 let connection = myModule.getConnection();
 
 module.exports = {
     getAllSchedulesByRoutes: function (routes, date) {
-        //return new Promise((resolve, reject) => {
-        // connection.query("SELECT * FROM schedules WHERE route_id = ? AND start_date <= ? AND end_date >= ?",
-        // [routes.id, date, date], (err, rows, fields) => {
-        //     if (err) {
-        //     reject(err);
-        //     } else {
-        //     resolve(rows);
-        //     }
-        // });
-        // });
+        let newSchedules;
         this.getAllSchedulesByRoutes1(routes, date).then((schedules) => {
+            for(let i = 0; i < schedules.length; i++){
+                dayOfWeek.getDaysOfWeekBySchedule(schedules[i]).then((days) => {
+                    schedules[i].days = days;
+                })
+            }
         });
     },
     getAllSchedulesByRoutes1: function (routes, date) { 
