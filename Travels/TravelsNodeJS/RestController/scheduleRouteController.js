cc2f57d609ec8dbd199e17e9948ce4ac6cff0774
Lara Mancuso <lara.mancuso@beije.it>
2023-10-16 09:58:58 +0200

    ok add schedule

--- Travels/TravelsNodeJS/RestController/scheduleRouteController.js
+++ Travels/TravelsNodeJS/RestController/scheduleRouteController.js
@@ -77,11 +77,21 @@ module.exports = {
     },
 
     addSchedule: function(scheduleDTO){
-        let scheduleId = scheduleRouteUtils.addSchedule(scheduleDTO);
-        for(let i=0; i<scheduleDTO.daysOfWeek.length; i++){
-            dayOfWeekUtils.addDayOfWeek(scheduleDTO.daysOfWeek[i], scheduleId);
-        }
-        return "ok";
+        return new Promise(async (resolve, reject) =>{
+            try{
+                let scheduleId = await scheduleRouteUtils.addSchedule(scheduleDTO);
+                console.log("Hello");
+                console.log(scheduleId);
+                
+                for(let i=0; i<scheduleDTO.daysOfWeek.length; i++){
+                    dayOfWeekUtils.addDayOfWeek(scheduleDTO.daysOfWeek[i], scheduleId);
+                }
+
+                resolve("ok");
+            }catch (error) {
+                reject(error);
+            }
+        } );
     }
 
     /*
