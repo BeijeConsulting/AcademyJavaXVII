a7df21b406bbe398b60244b09506fba8bbe3bae3
Flaviana <flaviana.caroselli@beije.it>
2023-10-16 09:49:40 +0200

    add xport

--- Travels/TravelsNodeJS/RestController/xportController.js
+++ Travels/TravelsNodeJS/RestController/xportController.js
@@ -3,5 +3,8 @@ const xportUtils = require('../Utils/xportUtils');
 module.exports = {
     getAllXports: function(){
         return xportUtils.getAllXports();
+    },
+    addXport: function(data) {
+        return xportUtils.addXport(data);
     }
 }
